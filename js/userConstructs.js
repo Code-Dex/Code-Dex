7f21c584696f26245a5b654818508aa5f2636b0b
Danni <DanielleNyxDaSilva@gmail.com>
2020-09-28 15:48:16 +0200

    Create userConstructs.js

---
+++ js/userConstructs.js
@@ -0,0 +1,88 @@
+function Users(name, surname, email, username, password) {
+    this.name = name;
+    this.surname = surname;
+    this.email = email;
+    this.username = username;
+    this.password = password;
+  }
+  
+  let user01 = new Users(
+    "Harry",
+    "Potter",
+    "JustHarry@hogwarts.com",
+    "SeekerOfSnitch",
+    "Hedw1g"
+  );
+  
+  let user02 = new Users(
+    "Hermione",
+    "Granger",
+    "LeviOsAH@hogwarts.com",
+    "BrightestWitchOHA",
+    "KnowledgeIsPower"
+  );
+  
+  let user03 = new Users(
+    "Ron",
+    "Weasley",
+    "CannonsFan0101@hogwarts.com",
+    "WeasleyIsOurKing",
+    "won-won-two-three"
+  );
+  
+  let user04 = new Users(
+    "Luna",
+    "Lovegood",
+    "SeerOfNargles@hogwarts.com",
+    "NargleHunter",
+    "W1tB3y0ndM3asur3"
+  );
+  
+  let user05 = new Users(
+    "Neville",
+    "Longbottom",
+    "HerbologyBoy@hogwarts.com",
+    "GreenThumb",
+    "MimbleMimbletona"
+  );
+  
+  let user06 = new Users(
+    "Draco",
+    "Malfoy",
+    "BetterThanYou@hogwarts.com",
+    "SlytherinPrince",
+    "PureBlood"
+  );
+  
+  let user07 = new Users(
+    "Minerva",
+    "Mcgonagall",
+    "TransfigurationMistress@hogwarts.com",
+    "MadamMinnie",
+    "M1lkisTheBest!"
+  );
+  
+  let user08 = new Users(
+    "Severus",
+    "Snape",
+    "PotionsMaster@hogwarts.com",
+    "MasterOfTheSubtleArts",
+    "Half-Blood-Pr1nce"
+  );
+  
+  let user09 = new Users(
+    "DontCallMeThat",
+    "Tonks",
+    "Auror.Tonks@DMLE.com",
+    "MetaMorphic",
+    "RemusLupinsGirl"
+  );
+  
+  let user10 = new Users(
+    "Pansy",
+    "Parkinson",
+    "AboveYourStation@hogwarts.com",
+    "PureBredPrincess",
+    "DrakeysGurl"
+  );
+  
